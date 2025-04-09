b0291b3d07b3b092d7be67ea7f060ffb58dba5b2
iborntowin <nassim28maaoui@gmail.com>
2025-04-09 11:38:15 +0100

    changed template

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\UX\Chartjs\ChartjsBundle::class => ['all' => true],
     Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
--- src/Controller/PackController.php
+++ src/Controller/PackController.php
@@ -29,14 +29,39 @@ class PackController extends AbstractController
         $newPacks = $this->packRepository->findBy([], ['id' => 'DESC'], 3);
 
         $packs = match ($filter) {
-            'trending' => $trendingPacks,
-            'new' => $newPacks,
+            'trending' => ['results' => $trendingPacks, 'currentPage' => 1, 'lastPage' => 1, 'hasPreviousPage' => false, 'hasNextPage' => false],
+            'new' => ['results' => $newPacks, 'currentPage' => 1, 'lastPage' => 1, 'hasPreviousPage' => false, 'hasNextPage' => false],
             default => $allPacks,
         };
 
         return $this->render('pack/index.html.twig', [
             'packs' => $packs,
             'trendingPacks' => $trendingPacks,
+            'currentFilter' => $filter
+        ]);
+    }
+
+
+    #[Route('/pack', name: 'app_pack_index')]
+    public function indexpack(Request $request): Response
+    {
+        $filter = $request->query->get('filter', 'all');
+        $page = $request->query->getInt('page', 1);
+        $limit = 10;
+        
+        $allPacks = $this->packRepository->findAllPaginated($page, $limit);
+        $trendingPacks = $this->packRepository->findBy([], ['prix' => 'DESC'], 3);
+        $newPacks = $this->packRepository->findBy([], ['id' => 'DESC'], 3);
+
+        $packs = match ($filter) {
+            'trending' => ['results' => $trendingPacks, 'currentPage' => 1, 'lastPage' => 1, 'hasPreviousPage' => false, 'hasNextPage' => false],
+            'new' => ['results' => $newPacks, 'currentPage' => 1, 'lastPage' => 1, 'hasPreviousPage' => false, 'hasNextPage' => false],
+            default => ['results' => $allPacks['results'], 'currentPage' => $allPacks['currentPage'], 'lastPage' => $allPacks['lastPage'], 'hasPreviousPage' => $allPacks['hasPreviousPage'], 'hasNextPage' => $allPacks['hasNextPage']],
+        };
+
+        return $this->render('pack/pack.html.twig', [
+            'packs' => $packs,
+            'trendingPacks' => ['results' => $trendingPacks, 'currentPage' => 1, 'lastPage' => 1, 'hasPreviousPage' => false, 'hasNextPage' => false],
             'currentFilter' => $filter
         ]);
     }
@@ -60,5 +85,42 @@ class PackController extends AbstractController
             'trendingPacks' => $this->packRepository->findBy([], ['prix' => 'DESC'], 3),
             'avis' => $avis
         ]);
+    }
+
+    #[Route('/pack/show/{id}', name: 'app_pack_show')]
+    public function show(Pack $pack): Response
+    {
+        return $this->render('pack/show.html.twig', [
+            'pack' => $pack
+        ]);
+    }
+
+    #[Route('/pack/edit/{id}', name: 'app_pack_edit')]
+    public function edit(Request $request, Pack $pack, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(PackType::class, $pack);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_pack_show', ['id' => $pack->getId()]);
+        }
+
+        return $this->render('pack/edit.html.twig', [
+            'pack' => $pack,
+            'form' => $form->createView()
+        ]);
+    }
+
+    #[Route('/pack/delete/{id}', name: 'app_pack_delete', methods: ['POST'])]
+    public function delete(Request $request, Pack $pack, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$pack->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($pack);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_packs');
     }
 }
